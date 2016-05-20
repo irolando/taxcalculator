ec2c5e0a23925d99baf675310f662276940c5e6a
Rolando Guerra <ay7x@hotmail.com>
2016-05-20 16:12:01 -0500

    Initial Commit

---
+++ TaxCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalculator
+//
+//  Created by Rolando Guerra on 5/20/16.
+//  Copyright © 2016 Rolando Guerra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TaxCalculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TaxCalculator
+//
+//  Created by Rolando Guerra on 5/20/16.
+//  Copyright © 2016 Rolando Guerra. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
